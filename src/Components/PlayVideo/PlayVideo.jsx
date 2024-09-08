6b40e76671a6d3ec2c43ed116efefcc08d5c9eea
ishaan-2201 <ishaanpuri2201@gmail.com>
2024-09-08 14:03:38 -0400

    Adding the project VidTube, which is a clone of YouTube

---
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -0,0 +1,130 @@
+import React, { useEffect, useState } from "react";
+import "./PlayVideo.css";
+import video1 from "../../assets/video.mp4";
+import like from "../../assets/like.png";
+import dislike from "../../assets/dislike.png";
+import share from "../../assets/share.png";
+import save from "../../assets/save.png";
+import jack from "../../assets/jack.png";
+import user_profile from "../../assets/user_profile.jpg";
+import { API_KEY, value_converter } from "../../data";
+import moment from "moment";
+import { useParams } from "react-router-dom";
+
+const PlayVideo = () => {
+  const {videoId}=useParams();
+  const [apiData, setApiData] = useState(null);
+  const [channelData, setChannelData] = useState(null);
+  const [commentData, setCommentData] = useState([]);
+
+  const fetchVideoData = async () => {
+    const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`;
+    await fetch(videoDetails_url)
+      .then((res) => res.json())
+      .then((res) => setApiData(res.items[0]));
+  };
+  const fetchOtherData = async () => {
+    const channelData_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`;
+    await fetch(channelData_url)
+      .then((res) => res.json())
+      .then((res) => setChannelData(res.items[0]));
+
+    const comment_url = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=50&videoId=${videoId}&key=${API_KEY}`;
+    await fetch(comment_url)
+      .then((res) => res.json())
+      .then((res) => setCommentData(res.items));
+  };
+  useEffect(() => {
+    fetchVideoData();
+  }, [videoId]);
+
+  useEffect(() => {
+    fetchOtherData();
+  }, [apiData]);
+
+  return (
+    <div className="play-video">
+      <iframe
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+      <h3>{apiData ? apiData.snippet.title : "Title here"}</h3>
+      <div className="play-video-info">
+        <p>
+          {apiData ? value_converter(apiData.statistics.viewCount) : "10K"}{" "}
+          views &bull;{" "}
+          {apiData ? moment(apiData.snippet.publishedAt).fromNow() : "Today"}
+        </p>
+        <div>
+          <span>
+            <img src={like} alt="" />
+            {apiData ? value_converter(apiData.statistics.viewCount) : 100}
+          </span>
+          <span>
+            <img src={dislike} alt="" />
+          </span>
+          <span>
+            <img src={share} alt="" />
+            Share
+          </span>
+          <span>
+            <img src={save} alt="" />
+            Save
+          </span>
+        </div>
+      </div>
+      <hr />
+      <div className="publisher">
+        <img
+          src={channelData ? channelData.snippet.thumbnails.default.url : ""}
+          alt=""
+        />
+        <div>
+          <p>{apiData ? apiData.snippet.channelTitle : ""}</p>
+          <span>
+            {channelData
+              ? value_converter(channelData.statistics.subscriberCount)
+              : 0}{" "}
+            subscribers
+          </span>
+        </div>
+        <button>Subscribe</button>
+      </div>
+      <div className="vid-description">
+        <p>
+          {apiData
+            ? apiData.snippet.description.slice(0, 250)
+            : "Description here"}
+        </p>
+        <hr />
+        <h4>
+          {apiData ? value_converter(apiData.statistics.commentCount) : 0}{" "}
+          Comments
+        </h4>
+        {commentData.map((comment) => (
+          <div className="comment" key={comment.id}>
+            <img src={comment.snippet.topLevelComment.snippet.authorProfileImageUrl} alt="" />
+            <div>
+              <h3>
+                {comment.snippet.topLevelComment.snippet.authorDisplayName} <span>{value_converter(moment(comment.snippet.topLevelComment.snippet.publishedAt).fromNow())}</span>
+              </h3>
+              <p>
+                {comment.snippet.topLevelComment.snippet.textDisplay}
+              </p>
+              <div className="comment-action">
+                <img src={like} alt="" />
+                <span>{value_converter(comment.snippet.topLevelComment.snippet.likeCount)}</span>    
+                <img src={dislike} alt="" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PlayVideo;
